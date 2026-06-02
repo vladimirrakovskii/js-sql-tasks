cc1f294f2323dcf9b9f24c0a41c83032c1ec9a72
vladimirrakovskii <v.rakovscky@yandex.ru>
2026-06-02 12:30:43 +0300

    Update solution.js

--- 2/solution.js
+++ 2/solution.js
@@ -8,5 +8,17 @@ const config = {
 };
 
 // BEGIN (write your solution here)
-
+const createTableWithData = async (data) => {
+  const sql = postgres(config);
+  const result = await sql`
+    INSERT INTO articles ${sql(data)} RETURNING id 
+  `;
+  await sql.end()
+  const ids = []
+  result.forEach((row) => {
+    ids.push(row.id);
+  })
+  return ids;
+};
+export default createTableWithData;
 // END
