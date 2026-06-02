acb9c1e5f5195a5f5795132bd68dacc350e78178
vladimirrakovskii <v.rakovscky@yandex.ru>
2026-06-02 12:32:16 +0300

    Update solution.js

--- 4/solution.js
+++ 4/solution.js
@@ -8,5 +8,30 @@ const config = {
 };
 
 // BEGIN (write your solution here)
+const transaction = async (user, roomNumber, price) => {
+  const sql = postgres(config);
+  try {
+    return await sql.begin(async (sql) => {
+      const [userInsertResult] = await sql`
+        INSERT INTO users ${sql(user)} RETURNING id
+      `;
+      const userId = userInsertResult.id;
 
+      const [roomSelectResult] = await sql`
+        SELECT id FROM rooms WHERE room_number = ${roomNumber}
+      `;
+      const roomId = roomSelectResult.id;
+
+      await sql`
+        INSERT INTO orders (user_id, room_id, price) 
+        VALUES (${userId}, ${roomId}, ${price})
+      `;
+
+      await sql`UPDATE rooms SET status = 'reserved' WHERE id = ${roomId}`;
+    });
+  } finally {
+    await sql.end();
+  }
+};
+export default transaction;
 // END
